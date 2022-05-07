a75ae25b7a79440744b7a38f9395b8dfc76695cd
NoAdsLol <103687072+noadslol@users.noreply.github.com>
2022-05-07 15:19:04 -0400

    Create dark.php

---
+++ p/dark.php
@@ -0,0 +1,14 @@
+
+echo '<style>
+body {
+  background-color: black;
+  color: white;
+}
+@media screen and (prefers-color-scheme: light) {
+  body {
+    background-color: white;
+    color: black;
+  }
+}
+
+</style>';
