8bbd8322e9c536b960d55d29ec5aa2b9c56b0480
Team Rawrware <103687072+noadslol@users.noreply.github.com>
2022-05-08 10:46:42 -0400

    Create likes.php

---
+++ p/likes.php
@@ -0,0 +1,10 @@
+if($_SERVER['PHP_SELF'] == '/watch.php' && isset($_GET['v'])){
+$rydapi = 'https://returnyoutubedislikeapi.com/votes?videoId='.$_GET['v'];
+$rydapi = @file_get_contents($rydapi);
+$rydapi = @json_decode($rydapi);
+if(isset($rydapi->viewCount)){
+echo '<p>This video has <b>'.$rydapi->viewCount.'</b> views, <b>'.$rydapi->likes.'</b> likes and <b>'.$rydapi->dislikes.'</b> dislikes. (Data powered by <a href="https://returnyoutubedislike.com/docs/usage-rights">RYD</a>,  usage rights apply.)</p>';
+}else{
+echo '<p>Cannot fetch data from RYD, server down or ratelimit exceeded.';
+}
+}
